54826fcbf51f430cb51a5ee81b8ab8a99fc3ee91
Paulschaber <paul.schaber@student.tabyenskilda.se>
2022-05-10 11:04:51 +0200

    js, conten

---
+++ script.js
@@ -0,0 +1,53 @@
+const lookupTable = [
+    ["0000FF", "#fc"],
+    ["e0203b", ""],
+    ["ff7e26", "#battery"],
+    ["00a4e7", "#camera"],
+    ["580281", "#rx"],
+    ["027d0b", "#esc"],
+    ["b5e61d", "#motor"],
+]
+
+const plusMinusRange = 10
+
+window.onload = function() {
+    const canvas = document.getElementById("explimg")
+    const ctx = canvas.getContext("2d")
+
+    canvas.width = canvas.getBoundingClientRect().width
+    canvas.height = canvas.getBoundingClientRect().height
+
+
+    img = new Image()
+    img.src = "img/Drone_Diagram.webp"
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+    }
+
+    canvas.onclick = function(event) {
+        const mouseX = event.x - canvas.getBoundingClientRect().left
+        const mouseY = event.y - canvas.getBoundingClientRect().top
+        console.log(mouseX, mouseY)
+        const color = ctx.getImageData(mouseX, mouseY, 1, 1).data
+        console.log(color)
+        console.log(color[0].toString(16) + color[1].toString(16) + color[2].toString(16))
+
+        lookupTable.forEach(cell => {
+            const colorHEX = cell[0]
+            const link = cell[1]
+
+            const r = parseInt(colorHEX.substring(0, 2), 16)
+            const g = parseInt(colorHEX.substring(2, 4), 16)
+            const b = parseInt(colorHEX.substring(4, 6), 16)
+
+            //console.log(r, g, b)
+
+            if (color[0] + plusMinusRange > r && color[0] - plusMinusRange < r &&
+                color[1] + plusMinusRange > g && color[1] - plusMinusRange < g &&
+                color[2] + plusMinusRange > b && color[2] - plusMinusRange < b) {
+                console.log("Match")
+                window.location = link
+            }
+        })
+    }
+}
