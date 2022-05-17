ae530f62610195f3603f07ddbd686124b9db4a84
Paulschaber <paul.schaber@student.tabyenskilda.se>
2022-05-17 11:00:28 +0200

    nya bilder, erkännande gallery, mq nya innehållet huvudsida

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//a table of the colors and the corresponding destinations
 const lookupTable = [
     ["0000FF", "#fc"],
     ["e0203b", ""],
@@ -8,8 +10,8 @@ const lookupTable = [
     ["b5e61d", "#motor"],
 ]
 
-const plusMinusRange = 10
 
+//drawing the image on a canvas and sizing it correctly
 window.onload = function() {
     const canvas = document.getElementById("explimg")
     const ctx = canvas.getContext("2d")
@@ -24,14 +26,16 @@ window.onload = function() {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
     }
 
+//getting the coordinates of the cursor, adjusting them to mach the coordinates of the canvas and get the color of this pixel when the canvas is clicked
     canvas.onclick = function(event) {
         const mouseX = event.x - canvas.getBoundingClientRect().left
         const mouseY = event.y - canvas.getBoundingClientRect().top
         console.log(mouseX, mouseY)
         const color = ctx.getImageData(mouseX, mouseY, 1, 1).data
         console.log(color)
-        console.log(color[0].toString(16) + color[1].toString(16) + color[2].toString(16))
+//        console.log(color[0].toString(16) + color[1].toString(16) + color[2].toString(16))
 
+//converting the hexadecimal values of the table into rgb
         lookupTable.forEach(cell => {
             const colorHEX = cell[0]
             const link = cell[1]
@@ -42,10 +46,14 @@ window.onload = function() {
 
             //console.log(r, g, b)
 
+            // the numeral range of margin for the colors
+            const plusMinusRange = 10
+
+//compare the color of the clicked pixel to the colors in the list
             if (color[0] + plusMinusRange > r && color[0] - plusMinusRange < r &&
                 color[1] + plusMinusRange > g && color[1] - plusMinusRange < g &&
                 color[2] + plusMinusRange > b && color[2] - plusMinusRange < b) {
-                console.log("Match")
+//              console.log("Match")
                 window.location = link
             }
         })
